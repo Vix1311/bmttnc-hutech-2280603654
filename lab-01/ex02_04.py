81bec7b65f73c398a2dc369ccfbe22b49d631e0c
Vix1311 <endy1311@gmail.com>
2025-05-13 15:03:41 +0700

    tìm tất cả các số chia hết cho 7 nhưng không là bội số của 5

--- lab-01/ex02_04.py
+++ lab-01/ex02_04.py
@@ -5,3 +5,4 @@ for i in range(2000, 3200):
     if (i % 7 == 0) and (i % 5 != 0):
         j.append(str(i))
 print(" ,".join(j))
+# nếu không dùng join() thì kết quả sẽ trông như này ['2002', '2009', '2016', ...]
