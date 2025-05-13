6556a80748941c46c404e000df69416b38a78cc6
Vix1311 <endy1311@gmail.com>
2025-05-13 16:00:52 +0700

    fix

--- lab-01/ex02_02.py
+++
@@ -1,7 +0,0 @@
-ban_kinh = float(input("Nhập bán kính hình tròn: "))
-dientich = 3.14 * (ban_kinh**2)
-# sử dụng .format() để chèn giá trị thông qua dấu {} 
-print ('Diện tích hình tròn với bán kính {} là: {}'.format(ban_kinh, dientich))
-# sử dụng f-string (formatted string literal) giúp biến được chèn trực tiếp vào trong chuỗi thông qua {}
-print (f'Diện tích hình tròn với bán kính {ban_kinh} là: {dientich}')
-
--- lab-01/ex02_03.py
+++
@@ -1,5 +0,0 @@
-so = int (input("Nhập 1 số nguyên: "))
-if so %2 ==0:
-    print ("{} là chẵn ".format(so))
-else:
-    print ("{} là lẻ".format(so))
