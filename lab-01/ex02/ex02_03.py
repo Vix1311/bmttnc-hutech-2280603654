575fb8672acc15158606e1e91eb8bffaed4daa04
MaiKyVi1311 <166972180+Vix1311@users.noreply.github.com>
2025-05-16 21:14:11 +0700

    Update ex02_03.py

--- lab-01/ex02/ex02_03.py
+++ lab-01/ex02/ex02_03.py
@@ -1,3 +1,7 @@
-banKinh = float(input("Nhập bán kính: "))
-dienTich = 3.14 *(banKinh**2)
-print(f"Diện tích của hình tròn là {dienTich}")
+#Nhập số từ người dùng 
+so = int(input("Nhập một số nguyên:"))
+#kiểm tra xem số đó có phải là số chẵn hay ko
+if so % 2 == 0:
+    print(so, "là số chẵn")
+else:
+    print(so, "không phải là số chẵn")
